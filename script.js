9c5a03ca41bb3ea37d0b9266309ae4c588019002
kndxiu <95412042+kndxiu@users.noreply.github.com>
2022-11-20 01:26:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@
 // https://airlabs.co/api/v9/fleets?airline_iata=${KOD LINI LOTNICZEJ}&api_key=28fa1de5-acf3-4093-9262-ca3c600330e9
 
 const table = document.getElementById("table");
-if(window.innerWidth < 1100){
+if(window.innerWidth > 1100){
 
 fetch('https://airlabs.co/api/v9/flights?api_key=28fa1de5-acf3-4093-9262-ca3c600330e9&bbox=49.0273953314,14.0745211117,54.8515359564,24.0299857927').then((response) => {
     return response.json()
